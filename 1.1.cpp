d14977d10d928d1b7ceb8ca2245bb8610eed41a7
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-11-04 00:49:22 +0500

    Create 1.1.cpp

---
+++ 1.1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1; //Обевляем i типа даных int
+    int a;//Обевляем a типа даных int
+    a = ++i * i++; //Считаем пример ++i=2 и i++=2 2*2=4
+    cout << a << endl; //Используя оператор вывода, выводим ответ
+    return 0;
+}
