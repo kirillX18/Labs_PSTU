1baa11749e497353cc080df4ba7c9cc6d675a7fc
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-11-09 15:55:23 +0500

    Create задание 3.cpp

---
+++ Алгоритмы не лабы/1.3/задание 3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a;
+    cin >> a;
+    if (a < 5)
+    {
+        cout << a * 3 << endl;
+    }
+    if ((a >= 5) and (a <= 7))
+    {
+        cout << a / 10 << endl;
+    }
+    if (a > 7)
+    {
+        cout << a + 3 << endl;
+    }
+    return 0;
+}
