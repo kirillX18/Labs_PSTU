4ee71ec0123ca0c0a7aed25892463398e314b512
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-11-09 15:55:07 +0500

    Create задаяча 1.cpp

---
+++ Алгоритмы не лабы/1.1/задаяча 1.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int a, b, c;
+    int max = 0;
+    cout << "Ведите три числа" << endl;
+    cin >> a >> b >> c;
+    if(a > max)
+    {
+        max = a;
+    }
+    if (b > max)
+    {
+        max = b;
+    }
+    if (c > max)
+    {
+        max = c;
+    }
+    cout << max << " максимальное" << endl;
+    return 0;
+
+}
