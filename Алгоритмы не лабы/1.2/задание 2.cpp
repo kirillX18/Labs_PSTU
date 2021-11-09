700132c834164617d88593619bd96bd20e991206
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-11-09 15:55:14 +0500

    Create задание 2.cpp

---
+++ Алгоритмы не лабы/1.2/задание 2.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    float a,b;
+    cin >> a >> b;
+    if (b == 0)
+    {
+        cout << "На 0 делить нельзя";
+    }
+    else
+    {
+        cout << a / b << endl;
+    }
+    return 0;
+}
