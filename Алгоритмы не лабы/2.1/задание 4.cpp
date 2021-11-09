ad4802f15b0da0e8465f1a65310e1c210214842c
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-11-09 15:55:28 +0500

    Create задание 4.cpp

---
+++ Алгоритмы не лабы/2.1/задание 4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int N; int S = 0;
+    cin >> N;
+    while (N != 0)
+    {
+        S = S + N;
+        N = N - 1;
+    }
+    cout << S << endl;
+    return 0;
+}
