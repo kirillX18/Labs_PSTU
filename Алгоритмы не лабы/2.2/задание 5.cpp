1e7dc88df2590699f34df2bba3fe43937d679e04
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-11-09 15:55:35 +0500

    Create задание 5.cpp

---
+++ Алгоритмы не лабы/2.2/задание 5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int N, S = 1;
+    cout << "Введите значение N натуральных чисел!" << endl;
+    cin >> N;
+    while (N != 0)
+    {
+        S = S * N;
+        N = N - 1;
+    }
+    cout << S << endl;
+    return 0;
+}
