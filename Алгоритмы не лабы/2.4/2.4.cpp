2e7a8e11d6a97d82d6accb5a9cea0cc115f7c4c0
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-11-20 14:57:54 +0500

    Create 2.4.cpp

---
+++ Алгоритмы не лабы/2.4/2.4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int spaces = n / 2;
+	int stars = 1;
+	for (int i = 1; i <= (n + 1) / 2; i++)
+	{
+		for (int j = 1; j <= spaces; j++)
+		{
+			cout << "  ";
+		}
+		spaces--;
+		for (int k = 1; k <= stars; k++)
+		{
+			cout << "* ";
+		}
+		stars += 2;
+		cout << endl;
+	}
+}
