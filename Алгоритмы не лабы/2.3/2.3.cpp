36242a835244b61f770835d566c971baddc95e7d
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-11-24 19:07:09 +0500

    Create 2.3.cpp

---
+++ Алгоритмы не лабы/2.3/2.3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int t, som = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        t = 1;
+        for(int j = i;j <= i * 2;j++)
+        {
+            t *= j;
+        }
+        som += t;
+    }
+cout << som << endl;
+}
+
+
