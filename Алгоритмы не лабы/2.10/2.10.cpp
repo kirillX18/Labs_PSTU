8da096d05b2139ce0c0136a31858a2c3a635d81b
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-11-27 14:24:53 +0500

    Create 2.10.cpp

---
+++ Алгоритмы не лабы/2.10/2.10.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int n;
+	int max = 0;
+	int num;
+	cout << "Введите длину последовательности:" << endl;
+	cin >> n;
+	for (int i = 0; i < n; i++) 
+	{
+		cout << "Введите число:" << endl;
+		cin >> num;
+		if (num > max) 
+		{
+			max = num;
+		}
+	}
+	cout << "Max элемент: " << max;
+}
