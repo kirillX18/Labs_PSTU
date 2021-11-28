70b923402f34b2d9871aa4cc64301cc815f7cd87
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-11-28 15:02:31 +0500

    Create 2.12.cpp

---
+++ Алгоритмы не лабы/2.12/2.12.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	setlocale(LC_ALL, "RUS");
+	int n;
+	int max = 0;
+	int min = 999999999;
+	int k = 0;
+	cout << "Введите длину последовательности" << endl;
+	cin >> n;
+	for (int i = 1; i <= n; i++) 
+	{
+		cout << "Введите число:" << endl;
+		cin >> k;
+		if (k > max) 
+		{
+			max = k;
+		}
+		if (k < min) 
+		{
+			min = k;
+		}
+	}
+	cout << "Сумма равна: " << (max + min);
+}
