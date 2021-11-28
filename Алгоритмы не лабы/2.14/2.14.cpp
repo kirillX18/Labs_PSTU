7c5886753520fccaf4656bbfa4d636a1bd81f74f
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-11-28 15:02:57 +0500

    Create 2.14.cpp

---
+++ Алгоритмы не лабы/2.14/2.14.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int n;
+	int s = 0;
+	cin >> n;
+	while (n > 0) 
+	{
+		s += n % 10;
+		n = n / 10;
+	}
+	cout << s << endl;
+	return 0;
+}
