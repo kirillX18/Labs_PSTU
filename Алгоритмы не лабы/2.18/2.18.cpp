52c68b81013a4f0684264ff31f068301b19de9fa
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-12-12 18:24:53 +0500

    Create 2.18.cpp

---
+++ Алгоритмы не лабы/2.18/2.18.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	setlocale(LC_ALL, "RUS");
+	int n, num;
+	num = 0;
+	cin >> n;
+	while (n > 0) 
+	{
+		num *= 10;
+		num = num + (n % 10);
+		n = n / 10;
+
+	}
+	cout << num << endl;
+	return 0;
+}
