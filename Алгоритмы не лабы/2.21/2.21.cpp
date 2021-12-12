355da31822901596d05b729efc7469a09c1f52bf
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-12-12 18:25:20 +0500

    Create 2.21.cpp

---
+++ Алгоритмы не лабы/2.21/2.21.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int n, v;
+	bool flag = true;
+	cin >> n;
+	while (n != 0)
+	{
+		v = n;
+		cin >> n;
+		if ((n != 0) && (n < v))
+		{
+			flag = false;
+		}
+	}
+	if (flag == true)
+	{
+		cout << "в порядке возрастания" << endl;
+	}
+	else
+	{
+		cout << "в не порядке возрастания" << endl;
+	}
+}
