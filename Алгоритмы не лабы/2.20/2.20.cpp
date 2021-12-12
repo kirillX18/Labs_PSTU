5ba705e12a35d24a34721d42be26f86780a91263
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-12-12 18:25:11 +0500

    Create 2.20.cpp

---
+++ Алгоритмы не лабы/2.20/2.20.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "RUS");
+    float n, p, a;
+    int i = 1;
+    bool flag = false;
+    cin >> n >> p;
+    while (i <= n && !flag) 
+    {
+        a = sin(n + i / n);
+        if (a == p) 
+        {
+            flag = true;
+        }
+        else 
+        {
+            i++;
+        }
+    }
+    if (flag) 
+    {
+        cout << "Элемент " << p << " Его номер: " << i << endl;
+    }
+    else 
+    {
+        cout << "Элемент не найден" << endl;
+    }
+    return 0;
+}
