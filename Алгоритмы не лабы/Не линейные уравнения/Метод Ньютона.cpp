271bf5f74849742159ce60acf6e7cd8843c2ce27
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-12-21 14:17:58 +0500

    Create Метод Ньютона.cpp

---
+++ Алгоритмы не лабы/Не линейные уравнения/Метод Ньютона.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+// f(x) = x^3 - x - 1
+// f'(x) = 3x^2 - 1
+// f''(x) = 6x
+// [1,2]
+// f(a) * f''(a) > 0;
+// or f(b) * f''(b) > 0 => x0 = b
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    float x, xPrev;
+    x = 2;
+    xPrev = 0;
+    double eps = 0.000001;
+    while (abs(x - xPrev) > eps) {
+        xPrev = x;
+        x = xPrev - ((pow(xPrev, 3) - xPrev - 1) / (3 * pow(xPrev, 2) - 1));
+    }
+    cout << "Корень: " << x << endl;
+    return 0;
+}
