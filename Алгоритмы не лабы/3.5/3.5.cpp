985b7cf007b90ecc1d732a9187b20fbf4d20607f
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-12-21 13:51:37 +0500

    Create 3.5.cpp

---
+++ Алгоритмы не лабы/3.5/3.5.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "RUS");
+    int a[5];
+    int count = 1;
+    int max = -10000;
+    for (int i = 0; i < 5; i++) 
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < 5; i++) 
+    {
+        if (a[i] > max) 
+        {
+            max = a[i];
+            count = 1;
+        }
+        else if (a[i] == max) 
+        {
+            count++;
+        }
+    }
+    cout << "Максимальный элемент: " << max << endl;
+    cout << "Количество элементов с этим значением: " << count << endl;
+    return 0;
+}
