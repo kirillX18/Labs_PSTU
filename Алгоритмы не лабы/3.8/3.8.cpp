515f031b3babe3b36cfc9a8c10e54b04b70a94b3
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-12-21 13:51:52 +0500

    Create 3.8.cpp

---
+++ Алгоритмы не лабы/3.8/3.8.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int  n,tmp;
+n = 11;
+int arr[11] = { 41,42,66,87,86,38,77,84,45,35,22 };
+for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+cout << endl;
+for (int i = 0; i < n / 2; i++)
+	{
+		tmp = arr[i];
+		arr[i] = arr[n - i - 1];
+		arr[n - i - 1] = tmp;
+	}
+for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+cout << endl;
+return = 0
+}
