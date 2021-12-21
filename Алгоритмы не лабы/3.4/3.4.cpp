ac8068aed22c272fa702886e4ff2be241e225a7f
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-12-21 13:51:26 +0500

    Create 3.4.cpp

---
+++ Алгоритмы не лабы/3.4/3.4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	setlocale(LC_ALL, "RUS");
+	int a[5];
+	int max = -10000;
+	for (int i = 0; i < 5; i++) 
+	{
+		cin >> a[i];
+	}
+	for (int i = 0; i < 5; i++) 
+	{
+		if (a[i] > max) 
+		{
+			max = a[i];
+		}
+	}
+	cout << "Максимальный элемент: " << max << endl;
+	return 0;
+}
