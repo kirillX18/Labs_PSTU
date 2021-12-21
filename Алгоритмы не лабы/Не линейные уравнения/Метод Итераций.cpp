865e823b0ecbc8f216f2aeed907937a7891f8689
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-12-21 14:18:03 +0500

    Create Метод Итераций.cpp

---
+++ Алгоритмы не лабы/Не линейные уравнения/Метод Итераций.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+// f(x) = x^3 - x - 1
+// f'(x) = 3x^2 - 1
+// f''(x) = 6x
+// [1,2]
+// ф(x) = x + l * (x^3 - x - 1)
+// -1/r < l < 0, если f'(x) > 0
+// 0 > l > 1/r, если f'(x) < 0
+// r = max(/f'(a)/, /f'(b)/)
+// l = -1/20 = -0.05
+int main() {
+    setlocale(LC_ALL, "RUS");
+    float x, xPrev;
+    double lambda = -0.05;
+    x = 2; // Можно выбрать любое x0 из выбранного заранее промежутка
+    xPrev = 0; // Делаем заведомо меньше x, чтобы цикл правильно сработал
+    double eps = 0.000001;
+    while (abs(x - xPrev) > eps) {
+        xPrev = x;
+        x = lambda * (pow(xPrev, 3) - xPrev - 1) + xPrev;
+    }
+    cout << "Корень: " << x << endl;
+    return 0;
+}
