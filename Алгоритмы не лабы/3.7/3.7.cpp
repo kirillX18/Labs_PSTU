1f14a8ebe6c731f5d4e1dd35c48d99f7084bc4c1
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-12-21 13:51:48 +0500

    Create 3.7.cpp

---
+++ Алгоритмы не лабы/3.7/3.7.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	setlocale(LC_ALL, "RUS");
+	int n = 7;
+	bool flag = true;
+	int a[7]{ 0, 1, 3, 8, 9, 12, 16 };
+	for (int i = 6; i > 0; i--) 
+	
+	{
+		if (a[i] < a[i - 1]) { flag = false; }
+	}
+	if (flag) {
+		cout << "Упорядоченый Масив" << endl;
+	}
+	else {
+		cout << "НЕ упорядоченый Мвсив" << endl;
+	}
+	return 0;
+}
