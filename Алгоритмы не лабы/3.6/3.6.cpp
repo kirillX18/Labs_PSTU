f8a2cd9fa22b890a26525b914e85652ee9926cad
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2021-12-21 13:51:42 +0500

    Create 3.6.cpp

---
+++ Алгоритмы не лабы/3.6/3.6.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	setlocale(LC_ALL, "RUS");
+	int max, min;
+	int a[10];
+	max =-10000;
+	min = 200000;
+	for (int i = 0; i < 10; i++) 
+	{
+				cin >> a[i];
+	}
+	for (int i = 0; i < 10; i++) 
+	{
+		if (a[i] > max) 
+		{
+			max = a[i];
+		}
+		else if (a[i] < min) 
+		{
+			min = a[i];
+		}
+	}
+	cout << "Максимальный элемент: " << max << endl;
+	cout << "Минимальный элемент: " << min << endl;
+	return 0;
+}
