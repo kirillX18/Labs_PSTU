a867e81e223d05750a7f1e2b10e9c3e423d328df
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2022-02-15 02:17:52 +0500

    Create 7.1.cpp

---
+++ 7.1/7.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int step(int a, int n)
+{
+	return pow(a, n);
+}
+
+double step(double a, double n)
+{
+	return pow(a,1/n);
+}
+
+int main()
+{
+	int a = 34;
+	int n = 5;
+	int d = step(a, n);
+	
+	double y = 34;
+	double u = 6;
+	double s = step(y, u);
+	cout << d << " " << s << endl;
+	return 0;
+
+}
