f04426b447da29b3baad9e0802fb733843553348
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2022-03-01 13:30:01 +0500

    Create Ханойские башни.cpp

---
+++ Ханойские башни/Ханойские башни.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Hanoi(int n, int k, int f, int t)
+{
+	if (n > 0)
+	{
+		Hanoi(n - 1, k, t, f);
+		cout << k << " => " << f << endl;
+		Hanoi(n - 1, t, f, k);
+	}
+	return 0;
+}
+
+int main()
+{
+	int n; 
+	cin >> n;
+	Hanoi(n, 1, 2, 3);
+	return 0;
+}
