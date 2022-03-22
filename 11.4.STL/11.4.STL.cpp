75d2d703281aa26aba37b991ea2f17a17f3a9793
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2022-03-22 15:03:06 +0500

    Create 11.4.STL.cpp

---
+++ 11.4.STL/11.4.STL.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include<queue>
+
+using namespace std;
+
+void printQueue(queue<int> S)
+{
+	int num = S.size();
+	for (int i = 0; i < num; i++)
+	{
+		cout << S.front() << " ";
+		S.pop();
+	}
+	cout << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	queue<int> S;
+	int n;
+	int k;
+	cout << "Впишите размер очередь" << endl;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> k;
+		S.push(k);
+	}
+	printQueue(S);
+	for (int i = 0; i < n; i++)
+	{
+		if (S.front() % 2 != 0)
+		{
+			S.push(S.front());
+		}
+		S.pop();
+	}
+	printQueue(S);
+}
