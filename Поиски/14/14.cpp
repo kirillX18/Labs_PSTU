1a1aaccb91630286227b5712cfee79f3fe7c0e84
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2022-03-29 15:53:14 +0500

    Create 14.cpp

---
+++ Поиски/14/14.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string s;
+    cout << "Впишите даные" << endl;
+    getline(cin, s);
+    int t;
+    t = s.size();
+    for (int i = 0; i < t; i++)
+    {
+        if (s[i] % 2 == 0)
+        {
+            s.erase(i, 1);
+            t--;
+            i--;
+        }
+    }
+    for (int i = 0; i < t; i++)
+    {
+        cout << s[i];
+    }
+    cout << endl; 
+}
