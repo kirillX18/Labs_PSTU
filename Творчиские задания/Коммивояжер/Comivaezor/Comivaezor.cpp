6844d5bbd9fd8768c57d1964823a61fd6a7921b5
KirillKoksharov <91093503+kirillX18@users.noreply.github.com>
2022-05-19 15:14:33 +0500

    Create Comivaezor.cpp

---
+++ Творчиские задания/Коммивояжер/Comivaezor/Comivaezor.cpp
@@ -0,0 +1,23 @@
+﻿#include "graph.h"
+#include <glut.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	graph = makeGraph();
+	graph.Print();
+	tsalesman(mat, n, help, result);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1350, 730);
+	glutCreateWindow("Graph");
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+	return 0;
+}
